79da93a5511607cf5c28e6b15b815dfd70482c73
Thanmayee167 <thanmayee167@gmail.com>
2025-03-01 14:10:34 +0530

    Created Spring Boot Started Project for web application

---
+++ src/test/java/com/thanmayee/SpringbootrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanmayee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
